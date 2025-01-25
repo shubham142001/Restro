e10caa1bcd8ed13464c605ba596637ac5021c0b5
Shubham <shubhamghadai@gmail.com>
2025-01-25 16:28:42 +0530

    sixth4 push

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -22,7 +22,7 @@ function Contactus() {
               <input
                 type="email"
                 id="email"
-                className="shadow-sm bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block lg:w-full  md:w-[600px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 m-auto"
+                className="shadow-sm bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block lg:w-full  md:w-[600px] p-2.5 m-auto"
                 placeholder="name@gmail.com"
                 required
               />

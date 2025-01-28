09a2f2db2b7786f4dfae06655db5ecf77ad3358d
Shubham <shubhamghadai@gmail.com>
2025-01-28 15:53:04 +0530

    ninth1 push

--- src/pages/Contactus1.jsx
+++ src/pages/Contactus1.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { Loader } from '../components/Loader'
 import Contactus from '../components/Contactus'
 
